54091108766471f97ffbf5ec5774974e0b90c33e
nstarman <nstarkman@protonmail.com>
2022-07-20 11:57:15 -0400

    add tests
    Signed-off-by: nstarman <nstarkman@protonmail.com>

---
+++ src/bound_class/descriptors/tests/test_base.py
@@ -0,0 +1,55 @@
+# STDLIB
+from abc import ABCMeta, abstractmethod
+
+# THIRD PARTY
+import pytest
+
+# LOCAL
+from bound_class.descriptors.base import InstanceDescriptorBase
+
+
+class InstanceDescriptorBase_Test(metaclass=ABCMeta):
+    @pytest.fixture
+    @abstractmethod
+    def descriptor_cls(self) -> type:
+        return InstanceDescriptorBase
+
+    @pytest.fixture
+    def enclosing_attr(self) -> str:
+        return "attr"
+
+    @pytest.fixture
+    def enclosing_cls(self, descriptor_cls, enclosing_attr) -> type:
+        Enclosing = type("Enclosing", (object,), {enclosing_attr: descriptor_cls()})
+
+        return Enclosing
+
+    @pytest.fixture
+    def descriptor_on_cls(self, enclosing_cls, enclosing_attr) -> object:
+        return vars(enclosing_cls)[enclosing_attr]
+
+    @pytest.fixture
+    def enclosing(self, enclosing_cls) -> object:
+        return enclosing_cls()
+
+    @pytest.fixture
+    def descriptor_on_inst(self, enclosing, enclosing_attr) -> object:
+        return getattr(enclosing, enclosing_attr)
+
+    # ===============================================================
+
+    def test_enclosing_attr_on_cls(self, descriptor_cls):
+        with pytest.raises(AttributeError):
+            descriptor_cls._enclosing_attr
+
+    def test_enclosing_attr_on_inst(self, descriptor_on_inst, enclosing_attr):
+        # This tests that __set_name__ was called
+        assert descriptor_on_inst._enclosing_attr == enclosing_attr
+
+    # -------------------------------------------
+
+    def test_replace(self, descriptor_on_inst):
+        newdescriptor = descriptor_on_inst._replace()
+
+        assert newdescriptor is not descriptor_on_inst
+        assert newdescriptor == descriptor_on_inst
